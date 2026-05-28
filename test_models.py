ebd417fe4f74f94379465140e323380ff820650c
João Palmeiro <joaommpalmeiro@gmail.com>
2026-05-28 20:43:04 +0100

    Update test models script

--- test_models.py
+++ test_models.py
@@ -1,4 +1,5 @@
 import supervision as sv
+from huggingface_hub import download_bucket_files
 from loguru import logger
 from PIL import Image
 from rfdetr import RFDETRMedium
@@ -19,6 +20,7 @@ def evaluate_medium_model() -> None:
     for image in TEST.glob("*.jpg"):
         detections = model.predict(str(image), threshold=0.5)
         logger.info("Number of predicted bounding boxes: {total}", total=len(detections.xyxy))
+        logger.info("Classes: {classes}", classes=detections.class_id)
 
         labels = [f"{COCO_CLASSES[class_id]}" for class_id in detections.class_id]
 
@@ -33,5 +35,44 @@ def evaluate_medium_model() -> None:
         logger.info("{image} evaluated", image=image.name)
 
 
+def evaluate_medium_trained_model() -> None:
+    ensure_clean_dir(RESULTS / "medium_trained")
+
+    download_bucket_files(
+        "joaompalmeiro/checkpoints",
+        files=[
+            ("medium-v1/checkpoint_best_total.pth", "./checkpoint_best_total.pth"),
+        ],
+    )
+
+    classes = sv.DetectionDataset.from_coco(
+        images_directory_path=TEST,
+        annotations_path=TEST / "_annotations.coco.json",
+    ).classes
+
+    model = RFDETRMedium(pretrain_weights="checkpoint_best_total.pth")
+    model.optimize_for_inference()
+
+    box_annotator = sv.BoxAnnotator()
+
+    for image in TEST.glob("*.jpg"):
+        detections = model.predict(str(image), threshold=0.5)
+        logger.info("Number of predicted bounding boxes: {total}", total=len(detections.xyxy))
+        logger.info("Classes: {classes}", classes=detections.class_id)
+
+        labels = [f"{classes[class_id]}" for class_id in detections.class_id]
+
+        annotated_image = box_annotator.annotate(
+            scene=detections.metadata["source_image"],
+            detections=detections,
+        )
+
+        Image.fromarray(annotated_image).save(RESULTS / "medium_trained" / image.name)
+        annotated_image = sv.LabelAnnotator().annotate(annotated_image, detections, labels)
+
+        logger.info("{image} evaluated", image=image.name)
+
+
 if __name__ == "__main__":
     evaluate_medium_model()
+    evaluate_medium_trained_model()
