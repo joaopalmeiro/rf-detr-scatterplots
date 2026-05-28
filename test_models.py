d7e8b565fb58a4098749a9f312d66a937e955b64
João Palmeiro <joaommpalmeiro@gmail.com>
2026-05-28 16:32:01 +0100

    Update test_models.py

--- test_models.py
+++ test_models.py
@@ -2,6 +2,7 @@ import supervision as sv
 from loguru import logger
 from PIL import Image
 from rfdetr import RFDETRMedium
+from rfdetr.assets.coco_classes import COCO_CLASSES
 
 from constants import RESULTS, TEST
 from utils import ensure_clean_dir
@@ -17,10 +18,13 @@ def evaluate_medium_model() -> None:
         detections = model.predict(str(image), threshold=0.5)
         logger.info("Number of predicted bounding boxes: {total}", total=len(detections.xyxy))
 
+        labels = [f"{COCO_CLASSES[class_id]}" for class_id in detections.class_id]
+
         annotated_image = box_annotator.annotate(
             scene=detections.metadata["source_image"],
             detections=detections,
         )
+        annotated_image = sv.LabelAnnotator().annotate(annotated_image, detections, labels)
 
         Image.fromarray(annotated_image).save(RESULTS / "medium" / image.name)
 
